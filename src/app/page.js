2eaadbe14bf75fae5ec17c2b741012b2c387e8d8
Tina Huynh <tinamchuynh@gmail.com>
2024-06-23 15:19:17 -0700

    update main.css and page.js update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import style from './../../public/main.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={style.main}>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
